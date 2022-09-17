600e096d8e639379db7950776e7143ec33b6f3ce
Sam H <samuelhussain@live.com>
2022-09-16 20:48:10 -0400

    Updates

---
+++ precache-manifest.766459dc828c846315a4227e86d59157.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c47b30c7ad8ec9f3f70",
+    "url": "/ReactCalc/static/css/main.df3c0426.chunk.css"
+  },
+  {
+    "revision": "7c47b30c7ad8ec9f3f70",
+    "url": "/ReactCalc/static/js/main.7c47b30c.chunk.js"
+  },
+  {
+    "revision": "749c875ada2a1b38ca6c",
+    "url": "/ReactCalc/static/js/1.749c875a.chunk.js"
+  },
+  {
+    "revision": "09bec062997f864dbc36",
+    "url": "/ReactCalc/static/js/runtime~main.09bec062.js"
+  },
+  {
+    "revision": "5ae8e749f545dcf1e74fd689dea213c8",
+    "url": "/ReactCalc/index.html"
+  }
+];
